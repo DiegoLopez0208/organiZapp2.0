62664f7b0a268b0f5bbd5d05cdedd91175cf5fc8
DiegoLopez0208 <lopezdiegog26@gmail.com>
2025-03-08 23:16:03 -0300

    chore: update log entries for client connection and disconnection events; format layout.jsx for consistency

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,11 +1,11 @@
-import Providers from "./providers/providers"; 
+import Providers from "./providers/providers";
 import "./globals.css";
 import NavBar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en' suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <body>
         <Providers>
           <NavBar />
